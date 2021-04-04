66744504c2c8ea6401e4ed8006ac5d6a130b4388
abisubramanya27 <abisubramanya27@gmail.com>
2021-04-05 00:52:21 +0530

    added img and video saving

--- partC/src/main.py
+++ partC/src/main.py
@@ -3,6 +3,7 @@ import time
 import argparse
 import numpy as np
 import seaborn as sns
+import os
 
 def load_input_image(image_path):
     test_img = cv2.imread(image_path)
@@ -91,8 +92,13 @@ def dectection_video_file(webcam, video_path, yolo_weights, yolo_cfg, coco_names
     frame_width = int(video.get(3))
     frame_height = int(video.get(4))
 
+    out_dir = os.path.join(os.path.dirname(video_path), '..', 'output')
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    out_name = os.path.splitext(os.path.basename(video_path))[0] + '.avi'
     fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    out = cv2.VideoWriter("output.avi", fourcc, 20.0, (frame_width, frame_height))
+    out = cv2.VideoWriter(os.path.join(out_dir, '..', out_name), fourcc, 20.0, (frame_width, frame_height))
+
     while True:
         ret, image = video.read()
         if ret == False:
@@ -113,6 +119,7 @@ def dectection_video_file(webcam, video_path, yolo_weights, yolo_cfg, coco_names
     out.release()
     cv2.destroyAllWindows()
 
+
 def detection_image_file(image_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold):
     img, h, w = load_input_image(image_path)
     net, classes, output_layers = yolov3(yolo_weights, yolo_cfg, coco_names)
@@ -121,7 +128,10 @@ def detection_image_file(image_path, yolo_weights, yolo_cfg, coco_names, confide
     boxes, confidences, class_ids = perform_detection(net, img, output_layers, w, h, confidence_threshold)
     final_img = draw_boxes_labels(boxes, confidences, class_ids, classes, img, colors, confidence_threshold, nms_threshold)
     cv2.imshow("Detection", final_img)
-    cv2.imwrite()
+    final_img_dir = os.path.join(os.path.dirname(image_path), '..', 'output')
+    if not os.path.exists(final_img_dir):
+        os.makedirs(final_img_dir)
+    cv2.imwrite("", os.path.join(final_img_dir, os.path.basename(image_path)))
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -133,9 +143,9 @@ if __name__ == '__main__':
     ap.add_argument('--video', help='Path to video file', default=None)
     ap.add_argument('--image', help='Path to the test images', default=None)
     ap.add_argument('--camera', help='To use the live feed from web-cam', type=bool, default=False)
-    ap.add_argument('--weights', help='Path to model weights', type=str, default='yolov3.weights')
-    ap.add_argument('--configs', help='Path to model configs',type=str, default='yolov3.cfg')
-    ap.add_argument('--class_names', help='Path to class-names text file', type=str, default='coco.names')
+    ap.add_argument('--weights', help='Path to model weights', type=str, default='../pre-trained-model/yolov3.weights')
+    ap.add_argument('--configs', help='Path to model configs',type=str, default='../pre-trained-model/yolov3.cfg')
+    ap.add_argument('--class_names', help='Path to class-names text file', type=str, default='../pre-trained-model/coco.names')
     ap.add_argument('--conf_thresh', help='Confidence threshold value', default=0.5)
     ap.add_argument('--nms_thresh', help='Confidence threshold value', default=0.4)
     args = vars(ap.parse_args())
@@ -144,6 +154,10 @@ if __name__ == '__main__':
     yolo_weights, yolo_cfg, coco_names = args['weights'], args['configs'], args['class_names']
     confidence_threshold = args['conf_thresh']
     nms_threshold = args['nms_thresh']
+
+
+    if image_path is None and args['camera'] == False and args['video'] is None:
+        print('No input provided to apply the model')
 
     if image_path:
         detection_image_file(image_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold)
