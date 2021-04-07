184537ed954c4de83e821677d087496b424348f5
abisubramanya27 <abisubramanya27@gmail.com>
2021-04-08 00:20:57 +0530

    added scores diplay or not option

--- partC/src/main.py
+++ partC/src/main.py
@@ -52,7 +52,7 @@ def perform_detection(net, img, output_layers, w, h, confidence_threshold):
     return boxes, confidences, class_ids
 
 
-def draw_boxes_labels(boxes, confidences, class_ids, classes, img, colors, confidence_threshold, NMS_threshold):
+def draw_boxes_labels(boxes, confidences, class_ids, classes, img, colors, confidence_threshold, NMS_threshold, disp_scores):
 
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, confidence_threshold, NMS_threshold)
 
@@ -65,7 +65,7 @@ def draw_boxes_labels(boxes, confidences, class_ids, classes, img, colors, confi
             color = [int(c) for c in colors[class_ids[i]]]
             cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
             # text = f"{class_ids[i]} -- {confidences[i]}"
-            text = "{}: {:.4f}".format(classes[class_ids[i]], confidences[i])
+            text = "{}: {:.4f}".format(classes[class_ids[i]], confidences[i]) if disp_scores else "{}".format(classes[class_ids[i]])
             (text_w, text_h) = cv2.getTextSize(text, FONT, fontScale=0.6, thickness=2)[0]
             text_offset_x, text_offset_y = 4, -4
             cv2.rectangle(img, (x-1, y-1), (x + text_offset_x + text_w + 4, y + text_offset_y - text_h - 5), color, cv2.FILLED)
@@ -74,31 +74,32 @@ def draw_boxes_labels(boxes, confidences, class_ids, classes, img, colors, confi
     return img
 
 
-def dectection_video_file(webcam, video_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold):
+def dectection_video_file(webcam, video_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold, disp_scores):
     net, classes, output_layers = yolov3(yolo_weights, yolo_cfg, coco_names)
     # colors = np.random.uniform(0, 255, size=(len(classes), 3))
     colors = ((np.array(sns.color_palette("husl", len(classes))) * 255)).astype(int)
 
     if webcam:
         video = cv2.VideoCapture(0)
-        time.sleep(2.0)
+        time.sleep(1.0)
         if (video.isOpened() == False): 
             print("Error opening camera")
             return
+        out_dir = os.path.join('..', 'output')
+        out_name = 'webcam_out' + '.avi'
     else:
         video = cv2.VideoCapture(video_path)
-    
         if (video.isOpened() == False): 
             print("Error reading video file")
             return
+        out_dir = os.path.join(os.path.dirname(video_path), '..', 'output')
+        out_name = os.path.splitext(os.path.basename(video_path))[0] + '.avi'
 
     frame_width = int(video.get(3))
     frame_height = int(video.get(4))
 
-    out_dir = os.path.join(os.path.dirname(video_path), '..', 'output')
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    out_name = os.path.splitext(os.path.basename(video_path))[0] + '.avi'
     fourcc = cv2.VideoWriter_fourcc(*"MJPG")
     out = cv2.VideoWriter(os.path.join(out_dir, out_name), fourcc, 20.0, (frame_width, frame_height))
 
@@ -110,7 +111,7 @@ def dectection_video_file(webcam, video_path, yolo_weights, yolo_cfg, coco_names
 
         h, w, _ = image.shape
         boxes, confidences, class_ids = perform_detection(net, image, output_layers, w, h, confidence_threshold)
-        final_img = draw_boxes_labels(boxes, confidences, class_ids, classes, image, colors, confidence_threshold, nms_threshold)
+        final_img = draw_boxes_labels(boxes, confidences, class_ids, classes, image, colors, confidence_threshold, nms_threshold, disp_scores)
         cv2.imshow("Detection", final_img)
         out.write(final_img)
 
@@ -123,14 +124,14 @@ def dectection_video_file(webcam, video_path, yolo_weights, yolo_cfg, coco_names
     cv2.destroyAllWindows()
 
 
-def detection_image_file(image_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold):
+def detection_image_file(image_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold, disp_scores):
     img, h, w = load_input_image(image_path)
     net, classes, output_layers = yolov3(yolo_weights, yolo_cfg, coco_names)
     # colors = np.random.uniform(0, 255, size=(len(classes), 3))
     colors = ((np.array(sns.color_palette("husl", len(classes))) * 255)).astype(int)
     boxes, confidences, class_ids = perform_detection(net, img, output_layers, w, h, confidence_threshold)
 
-    final_img = draw_boxes_labels(boxes, confidences, class_ids, classes, img, colors, confidence_threshold, nms_threshold)
+    final_img = draw_boxes_labels(boxes, confidences, class_ids, classes, img, colors, confidence_threshold, nms_threshold, disp_scores)
     
     cv2.imshow("Detection", final_img)
     final_img_dir = os.path.join(os.path.dirname(image_path), '..', 'output')
@@ -151,9 +152,9 @@ if __name__ == '__main__':
     ap.add_argument('--weights', help='Path to model weights', type=str, default='../pre-trained-model/yolov3.weights')
     ap.add_argument('--configs', help='Path to model configs',type=str, default='../pre-trained-model/yolov3.cfg')
     ap.add_argument('--class_names', help='Path to class-names text file', type=str, default='../pre-trained-model/coco.names')
-    ap.add_argument('--conf_thresh', help='Confidence threshold value', default=0.5)
-    ap.add_argument('--nms_thresh', help='Confidence threshold value', default=0.4)
-    ap.add_argument('-scores', help='To display probability scores for the object detected', action='store_true', default=False)
+    ap.add_argument('--conf_thresh', help='Confidence threshold value', default=0.6)
+    ap.add_argument('--nms_thresh', help='NMS threshold value', default=0.4)
+    ap.add_argument('-ds', help='To display probability scores for the object detected', action='store_true', default=False)
     args = vars(ap.parse_args())
 
     image_path = args['image']
@@ -170,11 +171,11 @@ if __name__ == '__main__':
         print('Two or more input sources provided. Give only one of image, video and webcam source')
 
     if image_path:
-        detection_image_file(image_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold)
+        detection_image_file(image_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold, args['ds'])
 
     elif args['camera'] == True or args['video']:
         webcam = args['camera']
         video_path = args['video']
-        dectection_video_file(webcam, video_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold)
+        dectection_video_file(webcam, video_path, yolo_weights, yolo_cfg, coco_names, confidence_threshold, nms_threshold, args['ds'])
 
 
